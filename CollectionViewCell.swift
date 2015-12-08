385a4187f09c5382dabd90c41a91eaae51f2d1c6
QiJi <Michael@qijis-macbook-pro.local>
2015-12-07 22:01:29 -0500

    Initial Commit

---
+++ CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewWaterfallLayoutDemo
+//
+//  Created by QiJi on 2015-12-01.
+//  Copyright © 2015 Eric Cerney. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
